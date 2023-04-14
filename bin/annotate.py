ec2f4f33e92d56e240dbb2b4652963e2fed7d8c7
Ahmed Said <ahmed.said@jax.org>
2023-04-14 12:04:23 -0400

    add comments to bin/annotate.py

--- bin/annotate.py
+++ bin/annotate.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 
-import scanpy as sc
-import pandas as pd
 from argparse import ArgumentParser
 from pathlib import Path
+
+import pandas as pd
+import scanpy as sc
 import scrublet as scr
 
+# Command line arguments should be pickled dataframe paths.
+# The dataframes contain genes, annotated by type
 parser = ArgumentParser()
 parser.add_argument('--ref_df_paths', '-r', nargs='*', type=Path, required=True)
 args = parser.parse_args()
 
+# The current directory should have cellranger outputs.
+# Get the filtered feature matrix, ensuring there is just one.
 filt_mat_paths = list(Path('.').glob('*filtered*matrix*.h5'))
 if len(filt_mat_paths) != 1:
     raise ValueError(
@@ -19,14 +24,22 @@ adata = sc.read_10x_h5(filt_mat_paths[0])
 
 # Iterate over each reference dataframe and each gene type and annotate
 for df_path in args.ref_df_paths:
-    df = pd.read_pickle(df_path)
-    gene_types = [col for col in df.columns if df[col].dtype == bool]
+    ref_df = pd.read_pickle(df_path)
+
+    # The boolean columns of the reference dataframes are the gene
+    # annotations, so get them with generator comprehension
+    gene_types = (col for col in ref_df.columns if ref_df[col].dtype == bool)
+    
+    # Iterate over each gene type, creating a column in adata for each
+    # that stores a boolean for whether the gene is of that type or not
     for gene_type in gene_types:
         adata.var[gene_type] = (
-            adata.var['gene_ids']
-            .str.upper()
-            .isin(df.loc[df[gene_type], 'ensembl_gene_id'])
-        )
+            adata.var['gene_ids']  # Match by gene ID
+            .str.upper()  # Convert gene IDs to uppercase
+            .isin(ref_df.loc[ref_df[gene_type], 'ensembl_gene_id'])
+        )  # If the gene ID matches those in the gene type column of
+           # the reference dataframe, mark the gene as true for that
+           # gene type
 
 # Make genes/cells names unique
 adata.var_names_make_unique()
